c23cf8be56600cd0ecadf11436a7dd85ea32cf45
rafaeczk <rafal.gabriel.kr@gmail.com>
2026-07-24 22:11:13 +0200

    friend codes fix

--- Application/Modules/FriendCodes/GetMyCode/GetMyCodeHandler.cs
+++ Application/Modules/FriendCodes/GetMyCode/GetMyCodeHandler.cs
@@ -1,8 +1,10 @@
-﻿using Domain.Common;
-using Application.Common.Interfaces;
+﻿using Application.Common.Interfaces;
+using Application.Modules.Debts.ChangeDebtAmount;
+using Domain.Common;
 using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Modules.FriendCodes.GetMyCode;
 
@@ -10,26 +12,46 @@ public record GetMyCodeCommand() : IRequest<Result<FriendCodeDTO>>;
 
 public class GetMyCodeHandler(
     IAppDbContext context,
-    IUserContext user) : IRequestHandler<GetMyCodeCommand, Result<FriendCodeDTO>>
+    IUserContext user, 
+    ILogger<ChangeDebtAmountHandler> logger) : IRequestHandler<GetMyCodeCommand, Result<FriendCodeDTO>>
 {
     public async Task<Result<FriendCodeDTO>> Handle(GetMyCodeCommand request, CancellationToken ct)
     {
-        var expiredCodes = await context.FriendCodes
-            .Where(c => c.ExpiresAt < DateTime.UtcNow)
-            .ToListAsync(ct);
+        using var transaction = await context.BeginTransactionAsync(ct);
 
-        context.FriendCodes.RemoveRange(expiredCodes);
+        try
+        {
+            await context.FriendCodes
+                .Where(c => c.ExpiresAt < DateTime.UtcNow)
+                .ExecuteDeleteAsync(ct);
 
-        var found = await context.FriendCodes.SingleOrDefaultAsync(c => c.UserId == user.Id, ct);
+            var found = await context.FriendCodes
+                .SingleOrDefaultAsync(c => c.UserId == user.Id, ct);
 
-        if (found is not null)
-            return new FriendCodeDTO(found.Code, found.ExpiresAt);
+            if (found is not null)
+            {
+                await transaction.CommitAsync(ct);
+                return new FriendCodeDTO(found.Code, found.ExpiresAt);
+            }
 
-        var friendCode = FriendCode.ForUser(user.Id);
+            var friendCode = FriendCode.ForUser(user.Id);
+            context.FriendCodes.Add(friendCode);
+            await context.SaveChangesAsync(ct);
 
-        context.FriendCodes.Add(friendCode);
-        await context.SaveChangesAsync(ct);
+            await transaction.CommitAsync(ct);
+            return new FriendCodeDTO(friendCode.Code, friendCode.ExpiresAt);
+        }
+        catch (Exception exception)
+        {
+            try
+            {
+                await transaction.RollbackAsync(ct);
+            }
+            catch { }
 
-        return new FriendCodeDTO(friendCode.Code, friendCode.ExpiresAt);
+            logger.LogError(exception, "Getting friend code error: UserId={UserId}", user.Id);
+
+            return Result.Failure("Technical error", FailureReason.InternalError);
+        }
     }
 }
